4bb4b4665c1f07ba3c80d1f7612a59c59ddb079e
pruthvi-21 <pruthvishankar397@gmail.com>
2022-03-01 16:41:07 +0530

    Applied dark mode as default

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { darkTheme } from "./utils/AppThemes";
+
 export default function App() {
-	return <div></div>;
+	const defaultTheme = darkTheme;
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<CssBaseline />
+		</ThemeProvider>
+	);
 }
